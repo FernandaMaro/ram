f1daff7032c7c10acc9440af11d2f818574654df
Fernanda Maro <148138579+FernandaMaro@users.noreply.github.com>
2024-07-16 18:55:36 -0600

    Css

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,42 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = ({ setAuth }) => {
-    const [usuario, setUser] = useState('');
-    const [contraseña, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [usuario, setUser] = useState("");
+  const [contraseña, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        if (usuario === 'Rick' && contraseña === 'Morty') {
-            setAuth(true);
-            navigate('/home');
-        } else {
-            alert('Usuario o contraseña incorrectos');
-        }
-    };
+  const handleLogin = () => {
+    if (usuario === "Rick" && contraseña === "Morty") {
+      setAuth(true);
+      navigate("/home");
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  };
 
-    return (
+  return (
+    <main>
+      <section className="container">
         <div className="login">
-            <h2>Inicia sesión</h2>
-            <input
-                type="text"
-                placeholder="Usuario"
-                value={usuario}
-                onChange={(e) => setUser(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Contraseña"
-                value={contraseña}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Ingresar</button>
+          <h2>Inicia sesión</h2>
+          <input
+            type="text"
+            placeholder="Usuario"
+            value={usuario}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={contraseña}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Ingresar</button>
         </div>
-    );
+      </section>
+    </main>
+  );
 };
 
 export default Login;
